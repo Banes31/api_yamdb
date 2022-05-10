cca533e47ee3dee6bd9c3b8ec9e91596486fc899
Kirill Grigorichev <grigorichev@gmail.com>
2022-05-10 17:14:31 +0300

    Создал приложение reviews. Создал  модели Review и Comment

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,79 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from users.models import User
+
+
+class Review(models.Model):
+    """Модель отзыва."""
+    title = models.ForeignKey(
+        to=Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Название произведения'
+    )
+    text = models.TextField(
+        verbose_name='Текст отзыва'
+    )
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор отзыва'
+    )
+    score = models.PositiveSmallIntegerField(
+        verbose_name='Оценка произведения',
+        validators=(
+            MaxValueValidator(
+                10, 'Максимальная оценка <= 10.'),
+            MinValueValidator(
+                1, 'Минимальная оценка >= 1.'),
+        ),
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата отзыва'
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ('-pub_date', 'title', '-score', 'text')
+        constraints = (
+            models.UniqueConstraint(
+                fields=('title', 'author'),
+                name='unique_review'),
+        )
+
+    def __str__(self):
+        return f'{str(self.author)}: {str(self.score)} - {str(self.title)}'
+
+
+class Comment(models.Model):
+    """Модель комментария к отзыву."""
+    review = models.ForeignKey(
+        to=Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв'
+    )
+    text = models.TextField(verbose_name='Текст комментария')
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор комментария'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата комментария'
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('-pub_date', '-review', '-id',)
+
+    def __str__(self):
+        return f'{self.author}: {self.text[:15]}'
