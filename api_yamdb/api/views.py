3f5243cc44399e25a092dab2f5f723391e6d6924
Banes31 <96978037+Banes31@users.noreply.github.com>
2022-05-15 12:06:47 +0300

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,21 +9,34 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import (AllowAny, IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
+from rest_framework.permissions import (
+  AllowAny,
+  IsAuthenticated,
+  IsAuthenticatedOrReadOnly
+)
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from reviews.models import Category, Comment, Genre, Review, Title, User
 
 from .mixins import CustomViewSet
-from .permissions import (AdminOnly, IsAdminOrReadOnly,
-                          IsAuthorOrAdminOrModeratorOrReadOnly)
-from .serializers import (AdminSerializer, CategorySerializer,
-                          CommentSerializer, GenreSerializer,
-                          GetTokenSerializer, ReviewSerializer,
-                          SignUpSerializer, TitleReadSerializer,
-                          TitleWriteSerializer, UsersSerializer)
+from .permissions import (
+  AdminOnly,
+  IsAdminOrReadOnly,
+  IsAuthorOrAdminOrModeratorOrReadOnly
+)
+from .serializers import (
+  AdminSerializer,
+  CategorySerializer,
+  CommentSerializer,
+  GenreSerializer,
+  GetTokenSerializer,
+  ReviewSerializer,
+  SignUpSerializer,
+  TitleReadSerializer,
+  TitleWriteSerializer,
+  UsersSerializer
+)
 
 MIN_VALUE_CODE = 100000
 MAX_VALUE_CODE = 999999
